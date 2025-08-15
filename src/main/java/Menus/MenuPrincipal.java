3932b2622ab8b3b6bcdc08587075621c69467a14
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-15 15:14:53 -0300

    Abstraccion Menu

--- src/main/java/Menus/MenuPrincipal.java
+++ src/main/java/Menus/MenuPrincipal.java
@@ -2,34 +2,17 @@ package Menus;
 
 import java.util.Scanner;
 
-public class MenuPrincipal{
+public class MenuPrincipal extends Menu{
 
-
-    public void abrirMenu(){
-        boolean exit = false;
-        int opcion = 0;
-        while (!exit){
-            try{
-                opcion = elegirOpcionMenuPrincipal();
-                exit = ejecutarOperacionMenuPrincipal(opcion);
-            }
-            catch(Exception e){
-                System.out.println(e.getMessage());
-            }
-        }
-        return;
-    }
-    private int elegirOpcionMenuPrincipal() throws Exception {
-        Scanner scanner = new Scanner(System.in);
+    @Override
+     protected void mostrarTextoOpciones (){
         System.out.println("Elija una opción: ");
         System.out.println("1. Agregar juego");
         System.out.println("2. Ver juegos cargados");
         System.out.println("3. Salir");
-        int r = scanner.nextInt();
-        verificarValidez(r);
-        return r;
     }
-    private boolean ejecutarOperacionMenuPrincipal(Integer opcion) throws Exception {
+    @Override
+    protected boolean ejecutarOperacionMenuPrincipal(Integer opcion) throws Exception {
         switch (opcion){
             case 1:
                 break;
@@ -38,12 +21,8 @@ public class MenuPrincipal{
             case 3:
                 return true;
             default:
-                throw new Exception("Error: Opcion Invalida - Case fallado");
+                throw new Exception("Error: Opcion Invalida");
         }
         return false;
-    }
-    private void verificarValidez(int opcion) throws Exception {
-        if(opcion>3 || opcion<1)
-            throw new Exception("Error: Opcion invalida");
     }
 }
