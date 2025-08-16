fd1125558420df3077e6e47942f2844bb1a3ce0f
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-16 10:05:38 -0300

    Add config for H2

--- src/main/java/lprtt/Cliente.java
+++ src/main/java/lprtt/Cliente.java
@@ -5,14 +5,14 @@ import Menus.MenuPrincipal;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 
-public class Main {
+public class Cliente {
 
     public static void main(String[] args) {
         System.out.println("Programa iniciado!");
 
 
         //System.setProperty("logging.level.root", "OFF");
-        SpringApplication app = new SpringApplication(Main.class);
+        SpringApplication app = new SpringApplication(Cliente.class);
         app.setBannerMode(Banner.Mode.OFF);
         app.setLogStartupInfo(false);
         app.run();
