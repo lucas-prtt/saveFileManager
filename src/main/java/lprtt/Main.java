f3906a8cd95a9908f40fcbadf2efc7baef35dcac
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-16 08:54:42 -0300

    Añado Springboot y dependencia de H2

--- src/main/java/lprtt/Main.java
+++ src/main/java/lprtt/Main.java
@@ -2,12 +2,18 @@ package lprtt;
 
 
 import Menus.MenuPrincipal;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Programa iniciado!");
-
+        //System.setProperty("logging.level.root", "OFF");
+        SpringApplication app = new SpringApplication(Main.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.setLogStartupInfo(false);
+        app.run();
         new MenuPrincipal().abrirMenu();
     }
 
