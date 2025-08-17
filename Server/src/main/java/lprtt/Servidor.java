a7722b4a17754760e8012d3a1d7e408be91fd34e
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-17 12:43:52 -0300

    Iniciar server springboot

---
+++ Server/src/main/java/lprtt/Servidor.java
@@ -0,0 +1,21 @@
+package lprtt;
+
+import java.io.*;
+import java.net.URISyntaxException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servidor {
+    public static void main(String[] args) {
+        SpringApplication.run(Servidor.class, args);
+        try{
+            System.out.println("Presiona Enter para detener el servidor...");
+            System.in.read();
+            System.out.println("Servidor detenido.");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
