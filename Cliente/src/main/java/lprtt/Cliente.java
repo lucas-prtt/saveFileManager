5abec7767a6206c74340e9aa7181a9a73c133880
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-18 13:58:10 -0300

    Cliente inicia springboot

--- Cliente/src/main/java/lprtt/Cliente.java
+++ Cliente/src/main/java/lprtt/Cliente.java
@@ -1,25 +1,26 @@
 package lprtt;
 
 
-import Menus.MenuPrincipal;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Collections;
 
+@SpringBootApplication
 public class Cliente {
 
     public static void main(String[] args) {
-        System.out.println("Programa iniciado!");
-
+        System.out.println("Iniciando cliente...");
 
         //System.setProperty("logging.level.root", "OFF");
         SpringApplication app = new SpringApplication(Cliente.class);
         app.setBannerMode(Banner.Mode.OFF);
         app.setLogStartupInfo(false);
-        app.run();
+        app.setDefaultProperties(Collections.singletonMap("logging.level.root", "WARN"));
         System.out.println("Springboot configurado!");
+        app.run();
+        System.out.println("Springboot iniciado!");
 
-
-        new MenuPrincipal().abrirMenu();
         return;
     }
 
