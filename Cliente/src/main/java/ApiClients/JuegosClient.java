e39201dcdfaf551411a4b0e60c2ba116e44c48ed
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-19 21:08:57 -0300

    POST, DELETE y PATCH Client

--- Cliente/src/main/java/ApiClients/JuegosClient.java
+++ Cliente/src/main/java/ApiClients/JuegosClient.java
@@ -1,10 +1,7 @@
 package ApiClients;
 
 import Archivos.Archivo;
-import Juegos.Checkpoint;
-import Juegos.CheckpointDTO;
-import Juegos.Juego;
-import Juegos.Partida;
+import Juegos.*;
 import ServerManagment.ServerConnection;
 import ServerManagment.ServerManager;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +17,9 @@ public class JuegosClient {
     public JuegosClient(ServerManager serverManager){
         this.serverManager = serverManager;
     }
+
+    // GET
+
     public List<String> obtenerTitulosJuegos(ServerConnection servidor){
         return servidor.getWebClient().get().uri("/api/juegos").retrieve().bodyToMono(new ParameterizedTypeReference<List<String>>(){}).block();
     }
@@ -40,6 +40,45 @@ public class JuegosClient {
     }
     public List<Archivo> obtenerArchivosCheckpoint(ServerConnection servidor, String tituloJuego, String tituloPartida, String uuidCheckpoint){
         return servidor.getWebClient().get().uri("/api/juegos/" + tituloJuego + "/partidas/" + tituloPartida + "/checkpoints/"+ uuidCheckpoint + "/archivos").retrieve().bodyToMono(new ParameterizedTypeReference<List<Archivo>>() {}).block();
+    }
+
+    // POST
+
+    public Juego postearJuego(ServerConnection servidor, Juego juego) {
+        return servidor.getWebClient().post().uri("/api/juegos").bodyValue(juego).retrieve().bodyToMono(Juego.class).block();
+    }
+
+    public Partida postearPartida(ServerConnection servidor, String tituloJuego, Partida partida) {
+        return servidor.getWebClient().post().uri("/api/juegos/" + tituloJuego + "/partidas").bodyValue(partida).retrieve().bodyToMono(Partida.class).block();
+    }
+
+    public Checkpoint postearCheckpoint(ServerConnection servidor, String tituloJuego, String tituloPartida, Checkpoint checkpoint) {
+        return servidor.getWebClient().post().uri("/api/juegos/" + tituloJuego + "/partidas/" + tituloPartida + "/checkpoints").bodyValue(checkpoint).retrieve().bodyToMono(Checkpoint.class).block();
+    }
+
+    // DELETE
+
+    public void eliminarJuego(ServerConnection servidor, String tituloJuego) {
+        servidor.getWebClient().delete().uri("/api/juegos/" + tituloJuego).retrieve().toBodilessEntity().block();
+    }
+
+    public void eliminarPartida(ServerConnection servidor, String tituloJuego, String tituloPartida) {
+        servidor.getWebClient().delete().uri("/api/juegos/" + tituloJuego + "/partidas/" + tituloPartida).retrieve().toBodilessEntity().block();
+    }
+
+    public void eliminarCheckpoint(ServerConnection servidor, String tituloJuego, String tituloPartida, String uuidCheckpoint) {
+        servidor.getWebClient().delete().uri("/api/juegos/" + tituloJuego + "/partidas/" + tituloPartida + "/checkpoints/" + uuidCheckpoint).retrieve().toBodilessEntity().block();
+    }
+
+    // PATCH
+
+
+    public Juego patchearJuego(ServerConnection servidor, String tituloJuego, JuegoPatchDTO patchDTO) {
+        return servidor.getWebClient().patch().uri("/api/juegos/" + tituloJuego).bodyValue(patchDTO).retrieve().bodyToMono(Juego.class).block();
+    }
+
+    public Partida patchearPartida(ServerConnection servidor, String tituloJuego, String tituloPartida, PartidaPatchDTO patchDTO) {
+        return servidor.getWebClient().patch().uri("/api/juegos/" + tituloJuego + "/partidas/" + tituloPartida).bodyValue(patchDTO).retrieve().bodyToMono(Partida.class).block();
     }
 
 
